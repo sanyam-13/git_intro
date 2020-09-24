3b7eef917f852cae117da6dbbf819a9570f420b3
Sanyam Jain <sanyam17jain@gmail.com>
2020-09-25 00:15:56 +0530

    new changes

--- file01.js
+++ file01.js
@@ -1,11 +1,8 @@
 function greet(name) {
-<<<<<<< HEAD
     console.log('knock knock knock ' + name)
     console.log('knock knock knock ' + name)
     console.log('knock knock knock ' + name)
-=======
     console.log("hello " + name)
->>>>>>> gitlearning
 }
 
 greet('Sanyam')
